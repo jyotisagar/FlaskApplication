b736f6fb06b66209d0cac9e25b11edf229ac749a
jyoti <jsagarbit@gmail.com>
2019-09-20 13:00:03 +0530

    added layout.html

--- FlaskBlog/app.py
+++ FlaskBlog/app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Jyoti',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date': 'Jan 01, 2000'
+    },
+{
+        'author': 'Sagar',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date': 'Jan 02, 2000'
+    },
+    {
+        'author': 'Chowrasia',
+        'title': 'Blog post 3',
+        'content': 'Third post content',
+        'date': 'Jan 03, 2000'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Home page'
+    return render_template('home.html', posts=posts) #'Home page'
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about') #'This is about Page'
 
 
 @app.route('/hello')
@@ -14,11 +39,6 @@ def hello():
     # name = request.args.get("name", "World")
     # return f'Hello, {escape(name)}!'
     return '<h3>Hello World! Welcome to Flask World..</h3>'
-
-
-@app.route('/about')
-def about():
-    return 'This is about Page'
 
 
 if __name__ == '__main__':
