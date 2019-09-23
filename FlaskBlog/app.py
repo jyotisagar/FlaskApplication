027b18a7f691610dd840636c0c1f45f9e568bd73
jyoti <jsagarbit@gmail.com>
2019-09-23 17:11:31 +0530

    Registration Form

--- FlaskBlog/app.py
+++ FlaskBlog/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '27f7637817cf42cc3f0e54e42fbbbe04'
 
 posts = [
     {
@@ -9,7 +11,7 @@ posts = [
         'content': 'First post content',
         'date': 'Jan 01, 2000'
     },
-{
+    {
         'author': 'Sagar',
         'title': 'Blog post 2',
         'content': 'Second post content',
@@ -39,6 +41,18 @@ def hello():
     # name = request.args.get("name", "World")
     # return f'Hello, {escape(name)}!'
     return '<h3>Hello World! Welcome to Flask World..</h3>'
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
